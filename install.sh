43ec281c7240bed6d3ae9aead482a012bfd8571e
Joe Harrison <joseph.harrison@windstream.com>
2022-12-08 12:05:37 -0500

    improved installer

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-fonts="mononoki go-mono jetbrains-mono"
-
-# Clone neovim config
+# GLOBAL PARAMETERS
+FONTS="mononoki go-mono jetbrains-mono"
 ASTRONVIM_REPO=https://github.com/AstroNvim/AstroNvim
 MY_REPO=https://github.com/josephbharrison/nvim
 
@@ -10,15 +9,15 @@ MY_REPO=https://github.com/josephbharrison/nvim
 [[ $1 == "base" ]] && echo "Installing base astronvim only" && BASE_ONLY=true
 
 # Verify homebrew install
-echo -en "Checking prereqs: "
-res=$(brew list)
-if [[ $? -ne 0 ]]; then
-    echo "MISSING"
-    echo "Installing homebrew"
-    /bin/bash -c "NONINTERACTIVE=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    echo "OK"
-fi
+function check_prereqs(){
+    echo -en "Checking prereqs: "
+    res=$(brew list)
+    if [[ $? -ne 0 ]]; then
+        echo "MISSING"
+        echo "Installing homebrew: "
+        /bin/bash -c "NONINTERACTIVE=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+}
 
 function fail(){
     echo "FAIL"
@@ -33,35 +32,38 @@ function muddle(){
     echo "FAIL"
 }
 
-# Install dependencies
-res=$(brew list nvim)
-if [[ $? -ne 0 ]]; then
-    echo -en "Installing neovim: "
-    brew update &> /dev/null && brew install nvim &> /dev/null && ok || fail
-else
-    echo -en "Upgrading neovim: "
-    brew upgrade nvim &> /dev/null && ok || muddle
-fi
+function install_fonts(){
+    # Install fonts
+    echo -en "Installing fonts: "
+    for font in $FONTS
+    do
+        res=$(brew list --cask font-${font}-nerd-font &> /dev/null)
+        if [[ $? -ne 0 ]]; then
+            sudo rm -f /Users/work/Library/Fonts/${font}* &> /dev/null
+            brew tap homebrew/cask-fonts &> /dev/null && 
+            brew install --cask font-${font}-nerd-font &> /dev/null || return 1
+        fi
+    done
+    return 0
+}
 
-# Install fonts
-for font in $fonts
-do
-    echo -en "Checking font ${font}: "
-    res=$(brew list --cask font-${font}-nerd-font &> /dev/null)
+function install_neovim(){
+    # Install dependencies
+    res=$(brew list nvim)
     if [[ $? -ne 0 ]]; then
-        echo "MISSING"
-        echo -en "Installing font $font: "
-        sudo rm -f /Users/work/Library/Fonts/${font}* &> /dev/null
-        brew tap homebrew/cask-fonts &> /dev/null && 
-        brew install --cask font-${font}-nerd-font &> /dev/null && ok || fail
+        echo -en "Installing neovim: "
+        brew update &> /dev/null && brew install nvim &> /dev/null || return 1
     else
-        ok
+        echo -en "Updating neovim: "
+        brew upgrade nvim &> /dev/null || return 1
     fi
-done
-
+    return 0
+}
 
 # Configure NeoVim
 function configure_neovim(){
+    echo -en "Configuring neovim: "
+
     # Create base config if needed
     now=$(date +%s)
     config_dir=${HOME}/.config
@@ -71,27 +73,37 @@ function configure_neovim(){
     nvim_config_dir=${config_dir}/nvim
     [[ -d $nvim_config_dir ]] && mv -f $nvim_config_dir ${nvim_config_dir}.${now}.bak
 
-    
-    # Install nvim configuration
-    if [[ $BASE_ONLY != true ]];then
-        git clone $MY_REPO $nvim_config_dir || return 1
-    else
-        git clone $ASTRONVIM_REPO $nvim_config_dir || return 1
-    fi
-    
-    # Backup nvim lua 
+    # Backup nvim packages
     nvim_local_dir=${HOME}/.local/share/nvim
     nvim_site_dir=${nvim_local_dir}/site
     [[ -d $nvim_site_dir ]] && mv -f $nvim_site_dir ${nvim_site_dir}.${now}.bak
 
+    # Install nvim configuration
+    if [[ $BASE_ONLY != true ]];then
+        git clone $MY_REPO $nvim_config_dir &> /dev/null || return 1
+    else
+        git clone $ASTRONVIM_REPO $nvim_config_dir &> /dev/null || return 1
+    fi
+    
     return 0
 }
 
-echo -en "Configuring neovim: "
-configure_neovim &> /dev/null && ok || fail 
+function packer_sync(){
+    # HEADLESS INSTALL
+    echo -en "Updating packages: "
+    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' &> /dev/null &
+    sleep 5
+}
 
-# HEADLESS INSTALL
-# nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+function install(){
+    installers="check_prereqs install_fonts install_neovim configure_neovim packer_sync"
+    for installer in $installers
+    do
+        $installer && ok || fail
+    done
+}
+
+install
 
 echo
 echo "Installation complete, run:"
